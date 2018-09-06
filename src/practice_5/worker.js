379b6758c46ec2eeda243505768daca637216396
bfzheng <baofeng_zheng@163.com>
2018-09-06 20:57:07 +0800

    [practice 5] finish class person/student/workder and call person introduce function in subclass student and workder

--- src/practice_5/worker.js
+++ src/practice_5/worker.js
@@ -1,1 +1,16 @@
+import Person from "../../src/practice_5/person.js";
+
+class Worker extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+    }
+
+    introduce() {
+        return super.introduce() + ` I am a Worker. I have a job.`;
+    }
+
+}
+
+module.exports = Worker;
+
 
